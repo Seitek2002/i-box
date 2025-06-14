22159856dd06ae09f94821ee8a26bce54cb61d78
Seitek2002 <seitekbegaliev@gmail.com>
2025-06-14 17:03:45 +0600

    changed

--- src/pages/Scan/index.tsx
+++ src/pages/Scan/index.tsx
@@ -94,7 +94,7 @@ const Scan: FC = () => {
             </div>
           )}
         </div>
-        <button className='py-[15px] px-[30px] bg-[#875AFF] text-white rounded-[12px] mt-[30px]' onClick={() => navigate('/admin/login/')}>Вход для заведения</button>
+        <button className='py-[15px] px-[30px] bg-[#875AFF] text-white rounded-[12px] mt-[30px]' onClick={() => navigate('/admin/')}>Вход для заведения</button>
       </div>
     </>
   );
